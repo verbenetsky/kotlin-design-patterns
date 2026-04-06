771c5581a8310163063203ba759dd4fcfcc241b0
Andrii Verbenets <127051967+verbenetsky@users.noreply.github.com>
2026-04-06 09:33:12 +0200

    Create bad_example.kt

---
+++ structural/facade/bad_example.kt
@@ -0,0 +1,90 @@
+package org.example.facade
+
+data class GameData(
+    val path: String,
+    val mapDownloaded: Boolean = false,
+    val mapLoaded: Boolean = false,
+    val soundtrackLoaded: Boolean = false,
+    val modsLoaded: Boolean = false,
+    val multiplayerConnected: Boolean = false,
+    val onlineModeEnabled: Boolean = false,
+    val started: Boolean = false
+)
+
+class FileReader {
+    fun read(path: String): GameData {
+        println("Reading game files from: $path")
+        return GameData(path = path)
+    }
+}
+
+class DownloadMap {
+    fun downloadMap(gameData: GameData): GameData {
+        println("Downloading map...")
+        return gameData.copy(mapDownloaded = true)
+    }
+}
+
+class LoadMap {
+    fun loadMap(gameData: GameData): GameData {
+        println("Loading map...")
+        return gameData.copy(mapLoaded = true)
+    }
+}
+
+class LoadSoundTrack {
+    fun loadSoundTrack(gameData: GameData): GameData {
+        println("Loading soundtrack...")
+        return gameData.copy(soundtrackLoaded = true)
+    }
+}
+
+class LoadMods {
+    fun loadMods(gameData: GameData): GameData {
+        println("Loading mods...")
+        return gameData.copy(modsLoaded = true)
+    }
+}
+
+class ConnectMultiplayerMode {
+    fun connectMultiplayer(gameData: GameData): GameData {
+        println("Connecting to multiplayer...")
+        return gameData.copy(multiplayerConnected = true)
+    }
+}
+
+class TurnOnOnlineMode {
+    fun turnOnOnlineMode(gameData: GameData): GameData {
+        println("Turning on online mode...")
+        return gameData.copy(onlineModeEnabled = true)
+    }
+}
+
+class GameStarter {
+    fun startGame(gameData: GameData): GameData {
+        println("Starting game...")
+        return gameData.copy(started = true)
+    }
+}
+
+fun main() {
+    val fileReader = FileReader()
+    val downloadMap = DownloadMap()
+    val loadMap = LoadMap()
+    val loadSoundTrack = LoadSoundTrack()
+    val loadMods = LoadMods()
+    val connectMultiplayerMode = ConnectMultiplayerMode()
+    val turnOnOnlineMode = TurnOnOnlineMode()
+    val gameStarter = GameStarter()
+
+    var gameData = fileReader.read("game.exe")
+    gameData = downloadMap.downloadMap(gameData)
+    gameData = loadMap.loadMap(gameData)
+    gameData = loadSoundTrack.loadSoundTrack(gameData)
+    gameData = loadMods.loadMods(gameData)
+    gameData = connectMultiplayerMode.connectMultiplayer(gameData)
+    gameData = turnOnOnlineMode.turnOnOnlineMode(gameData)
+    gameData = gameStarter.startGame(gameData)
+
+    println("Game is ready: $gameData")
+}
