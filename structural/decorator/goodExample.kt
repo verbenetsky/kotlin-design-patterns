1a6fe478407c17ed22d701de40edc35d7d32b664
Andrii Verbenets <127051967+verbenetsky@users.noreply.github.com>
2026-04-18 19:19:05 +0200

    Create goodExample.kt

---
+++ structural/decorator/goodExample.kt
@@ -0,0 +1,41 @@
+package org.example.decorator
+
+import java.util.*
+import kotlin.time.Clock
+
+interface Logger {
+    fun log(message: String)
+}
+
+class ConsoleLogger : Logger {
+    override fun log(message: String) {
+        println(message)
+    }
+}
+
+abstract class LoggerDecorator(protected val logger: Logger) : Logger
+
+class UniqueIdLogger(logger: Logger) : LoggerDecorator(logger) {
+    override fun log(message: String) {
+        logger.log("${UUID.randomUUID()} $message")
+    }
+}
+
+class ThreadNameLogger(logger: Logger) : LoggerDecorator(logger) {
+    override fun log(message: String) {
+        logger.log("$message (on ${Thread.currentThread().name} thread)")
+    }
+}
+
+class DateTimeLogger(logger: Logger, private val clock: Clock = Clock.System) : LoggerDecorator(logger) {
+    override fun log(message: String) {
+        logger.log("[${clock.now()}] $message")
+    }
+
+}
+
+fun main() {
+    val logger = UniqueIdLogger(ThreadNameLogger(DateTimeLogger(ConsoleLogger())))
+
+    logger.log("Application initialized")
+}
